77619c64c6760c19cfb78cb134892a4e4d0fa4b0
Nathaline Santos <nathalinep@gmail.com>
2026-04-22 15:02:48 -0300

    aula 14

---
+++ proj10-aula14/Pessoa.cs
@@ -0,0 +1,8 @@
+namespace demo_video_14
+{
+    public class Pessoa
+    {
+        public string Name { get; set; }
+    }
+}
+
---
+++ proj10-aula14/Program.cs
@@ -0,0 +1,16 @@
+﻿int valor = 10;
+
+Console.WriteLine(valor); // 10
+
+int copiaValor = valor;
+copiaValor = 20;
+Console.WriteLine(copiaValor); // 20
+
+demo_video_14.Pessoa pessoa = new demo_video_14.Pessoa();
+pessoa.Name = "João";
+Console.WriteLine(pessoa.Name); // Joao
+
+demo_video_14.Pessoa copiaPessoa = pessoa;
+copiaPessoa.Name = "Maria";
+Console.WriteLine(pessoa.Name); // Maria
+
